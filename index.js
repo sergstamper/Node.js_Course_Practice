ec56259b4755caa81c75c3dd3185ca4f4075a5b4
Serhii Kabanov <serg.kabanoff@gmail.com>
2023-10-13 01:26:32 +0300

    Practice-01

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/health-check', (req, res) => {
+    res.json({ status: 'Server is running'});
+});
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Express Server API',
+            description: 'health-check endpoint',
+            version: '1.0.0',
+        },
+    },
+    apis: ['index.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /health-check:
+ *   get:
+ *     summary: Check if the server is running.
+ *     description: Returns a JSON response indicating the server is running.
+ *     responses:
+ *       200:
+ *         description: Server is running.
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: Server is running
+ */
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
